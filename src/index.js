d4287029db171064eed901cbb92a808d66e9c226
Hrishikesh Bharadwaj <48100950+easycase00@users.noreply.github.com>
2024-02-01 21:26:21 -0500

    First Commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors')
+const app= express();
+
+app.use(cors());
+
+const port = 3000;
+
+app.get('/', (req, res)=> {
+    res.send('Arithmetic service - Hello World!');
+});
+
+app.get('/add/:n/:m', (req, res) => {
+    const num1 = parseInt(req.params.n);
+    const num2 = parseInt(req.params.m);
+    const sum = num1 + num2;
+    res.json(sum);
+});
+
+app.listen(port);
